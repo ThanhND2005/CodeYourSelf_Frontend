748682bb7cb581b627dd1cd8327c43b8a807d153
ThanhND2005 <nguyendanht2005@gmail.com>
2026-04-17 17:44:43 +0700

    them api tao khoa hoc, lay thong bao da nhan

---
+++ src/services/AdminService.ts
@@ -0,0 +1,63 @@
+import api from "@/lib/axios";
+
+export const AdminServices ={
+    getStudents : async () =>{
+        const res = await api.get(`/admin/getStudents`,{withCredentials: true})
+        return res.data
+    },
+    deleteStudent : async (studentId : string) =>{
+        await api.patch(`/admin/deleteStudent/${studentId}`,{},{withCredentials: true})
+    },
+    getTeachers : async () => {
+        const res = await api.get(`/admin/getTeachers`,{withCredentials: true})
+        return res.data
+    },
+    deleteTeacher : async (teacherId: string) =>{
+        await api.patch(`/admin/deleteTeacher/${teacherId}`,{},{withCredentials: true})
+    },
+    getNotificaitons : async () =>{
+        const res = await api.get(`/admin/getNotifications`,{withCredentials: true})
+        return res.data
+    },
+    deleteNotification : async (notificationId: string) =>{
+        await api.patch(`/admin/deleteNotification/${notificationId}`,{},{withCredentials: true})
+    },
+    getWaitCourses : async () =>{
+        const res = await api.get(`/admin/getWaitCourses`,{withCredentials: true})
+        return res.data
+    },
+    getStudentBills : async () =>{
+        const res = await api.get(`/admin/getStudentBills`,{withCredentials: true})
+        return res.data
+    },
+    postSalary : async () =>{
+        await api.post(`/admin/postSalary`,{},{withCredentials: true})
+    },
+    getSalary : async () => {
+        const res = await api.get(`/admin/getSalary`,{withCredentials: true})
+        return res.data
+    },
+    deleteStudentBill : async (paymentId: string) =>{
+        await api.patch(`/admin/deleteStudentBill/${paymentId}`,{},{withCredentials: true})
+    },
+    acceptWaitCourse : async (courseId: string) =>{
+        await api.patch(`/admin/acceptWaitCourse/${courseId}`,{},{withCredentials:true})
+    },
+    denyWaitCourse : async (courseId: string) =>{
+        await api.delete(`/admin/denyWaitCourse/${courseId}`,{withCredentials: true})
+    },
+    postStudentBill : async (courseId: string, studentId: string, amount: number) =>{
+        await api.post(`/admin/postStudentBill`,{courseId,studentId,amount},{withCredentials: true})
+    },
+    getCourses : async () =>{
+        const res = await api.get(`/admin/getCourses`,{withCredentials: true})
+        return res.data
+    },
+    ReceiveNotification : async () =>{
+        const res = await api.get(`/admin/ReceiveNotification`,{withCredentials: true})
+        return res.data
+    },
+    postNotification : async (role: string, title: string, content: string) =>{
+        await api.post(`/admin/postNotification`,{role,title,content},{withCredentials: true})
+    }
+}
---
+++ src/stores/useAdminStore.ts
@@ -0,0 +1,22 @@
+import {type adminState, type Course, type DashboardNotificationDTO, type Notification, type Payment, type Student, type Teacher, type WaitCourse } from "@/types/admin";
+
+import { create } from "zustand";
+
+
+
+export const useAdminStore = create<adminState>((set,get)=>({
+    students : null,
+    teachers: null,
+    courses : null,
+    payments: null,
+    waitCourses :null,
+    receivedNotifications : null,
+    notifications: null,
+    setNotifications: (notifications : Notification[]) => set({notifications}),
+    setReceivedNotificatons : (receivedNotifications : DashboardNotificationDTO[]) => set({receivedNotifications}),
+    setWaitCourses : (waitCourses: WaitCourse[]) => set({waitCourses}),
+    setPayments: (payments : Payment[]) => set({payments}), 
+    setCourses : (courses : Course[]) => set({courses}),
+    setTeachers : (teachers : Teacher[]) => set({teachers}),
+    setStudents : (students : Student[]) => set({students})
+}))
